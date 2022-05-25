ae36b1854614113fbd5aeb6f603a506cf953f3f8
seenusneha4 <seenaannmathew6@gmail.com>
2022-05-25 19:20:00 +0530

    searching

--- src/Components/Searchbus.js
+++ src/Components/Searchbus.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Searchbus = () => {
+    var[route,setroute]=useState("")
+    const subdata=()=>{
+        const data={"route":route}
+        console.log(data)
+    }
   return (
-    <div>Searchbus</div>
+    <div>
+        <div className="container">
+    <div className="row div col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <label for="" className="form-label">Route</label>
+              <input onChange={(a)=>{setroute(a.target.value)}} type="text" className="form-control"/>
+            </div>
+           
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button onClick={subdata} className="btn btn-success">SEARCH</button>
+
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
   )
 }
 
